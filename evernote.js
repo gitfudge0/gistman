9dfd824c0cbce022358819594d78da2ff6c206a7
thediggu <thediggu@gmail.com>
2019-04-02 23:28:20 +0530

    Add note creation and minor changes
    * Add Note creation to a notebook
    * Fix Evernote Client authentication issue
    * Change Evernote Helper methods

--- evernote.js
+++ evernote.js
@@ -1,51 +1,120 @@
 const Evernote = require('evernote');
 
-const EVERNOTEAPP = {};
-
-EVERNOTEAPP.getEvernoteClient = (token = null) => new Promise(async (resolve, reject) => {
-  try {
-    let client;
-    if (token) {
-      client = await new Evernote.Client({
+/**
+ * @name getEvernoteClient
+ * @description Get an instance of Evernote Client
+ * @param {String} token Token to get authenticated client
+ * @returns Evernote client
+ */
+function getEvernoteClient(token) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const options = {
         token,
-      });
-    } else {
-      client = await new Evernote.Client({
-        consumerKey: 'thediggu',
-        consumerSecret: '3bd18451c6442d44',
-        sandbox: true, // change to false when you are ready to switch to production
+        sandbox: true,
         china: false,
-      });
+      };
+      const client = await new Evernote.Client(options);
+      resolve(client);
+    } catch (err) {
+      reject(err);
     }
+  });
+}
 
-    resolve(client);
-  } catch (err) {
-    reject(err);
-  }
-});
-
-EVERNOTEAPP.getEvernoteAccessToken = client => new Promise(async (resolve, reject) => {
-  try {
+/**
+ * @name getEvernoteAccessToken
+ * @description Get the token details required to get an instance of an authenticated client
+ * @param {*} client Evernote client to get acces token
+ * @returns token and secret
+ */
+function getEvernoteAccessToken(client) {
+  return new Promise(async (resolve, reject) => {
+    try {
     // Having callback url makes no sense
     // TODO: Figure out a way to remove the need for callback url
-    await client.getRequestToken('http://whyeven', (error, oauthToken, oauthTokenSecret) => {
-      if (error) {
-        reject(error);
+      await client.getRequestToken('http://whyeven', (error, oauthToken, oauthTokenSecret) => {
+        if (error) {
+          reject(error);
+        }
+        console.log({ oauthToken, oauthTokenSecret });
+        resolve({ oauthToken, oauthTokenSecret });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+
+/**
+ * @name getNoteStore
+ * @description Get an instance of the note store
+ * @param {*} client - Authenticated client
+ * @returns note store
+ */
+function getNoteStore(client) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const noteStore = await client.getNoteStore();
+      resolve(noteStore);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+/**
+ * @name getUserStore
+ * @des
+ * @param {*} client
+ * @returns
+ */
+function getUserStore(client) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const userStore = await client.getUserStore();
+      resolve(userStore);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+/**
+ * @name makeNote
+ * @description Makes a new note on the selected notebook
+ * @param {NoteStore} noteStore - Instance of note store
+ * @param {string} noteTitle - Title of the note
+ * @param {string} noteBody - Content of the note
+ * @param {Notebook} parentNotebook - Notebook in which note is created
+ * @returns
+ */
+function makeNote(noteStore, noteTitle, noteBody, parentNotebook) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const Note = Evernote.Types.Note();
+      Note.title = noteTitle;
+
+      const body = `<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">\n<en-note>${noteBody}</en-note>`;
+      Note.content = body;
+
+      if (parentNotebook && parentNotebook.guid) {
+        Note.notebookGuid = parentNotebook.guid;
       }
-      resolve({ oauthToken, oauthTokenSecret });
-    });
-  } catch (error) {
-    reject(error);
-  }
-});
 
-EVERNOTEAPP.getNoteStore = client => new Promise(async (resolve, reject) => {
-  try {
-    const noteStore = await client.getNoteStore();
-    resolve(noteStore);
-  } catch (error) {
-    reject(error);
-  }
-});
+      const createdNote = await noteStore.createNote(Note);
+      resolve(createdNote);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
-module.exports = EVERNOTEAPP;
+module.exports = {
+  getEvernoteClient,
+  getEvernoteAccessToken,
+  getNoteStore,
+  getUserStore,
+  makeNote,
+};
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const Evernote = require('./evernote');
 
+// Dev token url: https://sandbox.evernote.com/api/DeveloperToken.action
+const devToken = 'S=s1:U=953e8:E=1713699c2c8:C=169dee893d0:P=1cd:A=en-devtoken:V=2:H=150be53b2b29620fe2d360d93c81df54';
+
 async function main() {
-  const evernoteClient = await Evernote.getEvernoteClient();
-  const token = await Evernote.getEvernoteAccessToken(evernoteClient);
+  try {
+    // Get client
+    const authenticatedClient = await Evernote.getEvernoteClient(devToken);
 
-  // Get client with token
-  const authenticatedClient = await Evernote.getEvernoteClient(token.oauthToken);
+    // Get note store and get notebooks
+    const noteStore = await Evernote.getNoteStore(authenticatedClient);
+    const notebooks = await noteStore.listNotebooks();
 
-  // Get note store
-  const noteStore = await Evernote.getNoteStore(authenticatedClient);
-  console.log({ noteStore });
+    // Select a notebook
+    const selectedNote = notebooks.filter(curr => curr.name === 'GistNoteIt')[0];
+
+    // Make a new note
+    await Evernote.makeNote(noteStore, 'Test Title', 'Test content', selectedNote);
+  } catch (error) {
+    console.log(`\x1b[31mError caught\x1b[0m: ${JSON.stringify(error)}`);
+    console.trace(error);
+  }
 }
 
 main();
