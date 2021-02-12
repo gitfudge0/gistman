fcb6c121c22ba722b9235498ed059f0801830116
thediggu <thediggu@gmail.com>
2021-02-13 01:32:43 +0530

    feat: Add gist creation

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const printer = require('./lib/printer');
 const github = require('./lib/github');
+const CONSTANTS = require('./lib/constants');
 
 async function main() {
+  let userAction;
   printer.printIntroductions();
   
   try {
     await github.initUser();
-    await github.doUserAction();
+
+    do {
+      userAction = await github.doUserAction();
+    } while (userAction !== CONSTANTS.USER_ACTIONS.EXIT);
   } catch (error) {
     printer.printError(error);
   }
